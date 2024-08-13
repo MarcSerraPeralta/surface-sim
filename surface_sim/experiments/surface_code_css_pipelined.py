730b669a70961bbebc1e4c306461af1ec588a787
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-08-13 16:20:21 +0200

    Add `QUBIT_COORDS` to the experiments (#8)

--- surface_sim/experiments/surface_code_css_pipelined.py
+++ surface_sim/experiments/surface_code_css_pipelined.py
@@ -3,7 +3,12 @@ from stim import Circuit
 
 from qec_util import Layout
 
-from ..circuit_blocks.surface_code_css_pipelined import init_qubits, log_meas, qec_round
+from ..circuit_blocks.surface_code_css_pipelined import (
+    init_qubits,
+    log_meas,
+    qec_round,
+    qubit_coords,
+)
 from ..models import Model
 
 
@@ -23,6 +28,7 @@ def memory_experiment(
 
     num_init_rounds = 1 if meas_reset else 2
 
+    qubit_coords_circ = qubit_coords(model, layout)
     init_circ = init_qubits(model, layout, data_init, rot_basis)
     meas_circuit = log_meas(model, layout, rot_basis, meas_reset)
 
@@ -32,7 +38,8 @@ def memory_experiment(
         qec_circ = qec_round(model, layout, meas_reset)
 
         experiment = (
-            init_circ
+            qubit_coords_circ
+            + init_circ
             + first_qec_circ * num_init_rounds
             + qec_circ * (num_rounds - num_init_rounds)
             + meas_circuit
