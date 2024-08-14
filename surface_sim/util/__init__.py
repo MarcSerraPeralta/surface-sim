b1fe0c8dd07d5fdd3e16b0e6bbcfaca1891d4baa
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-08-14 16:49:51 +0200

    Add `get_circuit_distance` in `util` (#20)

--- surface_sim/util/__init__.py
+++ surface_sim/util/__init__.py
@@ -1,3 +1,4 @@
 from .data_gen import sample_memory_experiment
+from .distance import get_circuit_distance
 
-__all__ = ["sample_memory_experiment"]
+__all__ = ["sample_memory_experiment", "get_circuit_distance"]
