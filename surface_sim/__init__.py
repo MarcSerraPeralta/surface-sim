d46fc9bea5271c02e587a333d35294e1e5d73281
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-09-16 16:39:38 +0200

    Move `layouts` module from `qec-util` to `surface-sim` (#49)

--- surface_sim/__init__.py
+++ surface_sim/__init__.py
@@ -2,15 +2,18 @@
 
 __version__ = "0.1.0"
 
-from . import experiments, models, util, circuit_blocks
+from . import experiments, models, util, circuit_blocks, layouts
 from .setup import Setup
 from .models import Model
+from .layouts import Layout
 
 __all__ = [
     "models",
     "experiments",
     "util",
     "circuit_blocks",
+    "layouts",
     "Setup",
     "Model",
+    "Layout",
 ]
