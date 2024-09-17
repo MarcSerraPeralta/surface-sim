ee23afc3c927dc43aacf399722e1a8ab7a18e169
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-09-17 11:31:33 +0200

    Add `Detectors` class (#54)

--- surface_sim/__init__.py
+++ surface_sim/__init__.py
@@ -5,6 +5,7 @@ __version__ = "0.1.0"
 from . import experiments, models, util, circuit_blocks, layouts
 from .setup import Setup
 from .models import Model
+from .detectors import Detectors
 from .layouts import Layout
 
 __all__ = [
@@ -15,5 +16,6 @@ __all__ = [
     "layouts",
     "Setup",
     "Model",
+    "Detectors",
     "Layout",
 ]
