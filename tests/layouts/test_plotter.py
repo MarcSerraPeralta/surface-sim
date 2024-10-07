13e96437bbe693f105394332c90a516b66bd109d
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-10-07 15:41:30 +0200

    Add fontsize option in layout plotter

--- tests/layouts/test_plotter.py
+++ tests/layouts/test_plotter.py
@@ -15,3 +15,16 @@ def test_plot(show_figures):
     plt.close()
 
     return
+
+
+def test_plot_large_code(show_figures):
+    _, ax = plt.subplots()
+    layout = rot_surf_code(7)
+
+    plot(ax, layout, label_fontsize=5)
+
+    if show_figures:
+        plt.show()
+    plt.close()
+
+    return
