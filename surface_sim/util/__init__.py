0168376626c8a2180c68c8a27820bc6b01e11c57
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-10-15 20:08:43 +0200

    Add merge_circuits

--- surface_sim/util/__init__.py
+++ surface_sim/util/__init__.py
@@ -1,3 +1,4 @@
 from .data_gen import sample_memory_experiment
+from .circuit_operations import merge_circuits
 
-__all__ = ["sample_memory_experiment"]
+__all__ = ["sample_memory_experiment", "merge_circuits"]
