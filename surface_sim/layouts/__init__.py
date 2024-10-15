f64b7825356533a55f18818de45d05e8e9c23d8a
MarcSerraPeralta <marcserraperalta@gmail.com>
2024-10-15 22:58:31 +0200

    Add check for non-overlapping layouts

--- surface_sim/layouts/__init__.py
+++ surface_sim/layouts/__init__.py
@@ -2,5 +2,13 @@ from .layout import Layout
 from .library import rot_surf_code, rot_surf_code_rectangle
 from .plotter import plot
 from .util import set_coords
+from .operations import check_non_overlapping_layouts
 
-__all__ = ["Layout", "rot_surf_code", "rot_surf_code_rectangle", "plot", "set_coords"]
+__all__ = [
+    "Layout",
+    "rot_surf_code",
+    "rot_surf_code_rectangle",
+    "plot",
+    "set_coords",
+    "check_non_overlapping_layouts",
+]
---
+++ tests/layouts/test_operations.py
@@ -0,0 +1,22 @@
+import pytest
+import networkx as nx
+
+from surface_sim.layouts.operations import check_non_overlapping_layouts
+from surface_sim.layouts import rot_surf_code
+
+
+def test_check_non_overlapping_layouts():
+    layout_1 = rot_surf_code(1)
+    layout_2 = rot_surf_code(2)
+    layout_2._log_qubits = ["L1"]
+    layout_2._qubit_inds = {"D2": 10}
+    graph = nx.DiGraph()
+    graph.add_node("D2", coords=(11, 11))
+    layout_2.graph = graph
+
+    check_non_overlapping_layouts(layout_1, layout_2)
+
+    with pytest.raises(ValueError):
+        check_non_overlapping_layouts(layout_1, layout_1)
+
+    return
