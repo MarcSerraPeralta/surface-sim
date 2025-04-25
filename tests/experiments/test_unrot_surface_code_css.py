028550654715126232156c059c6ec5c309dab8b4
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2025-04-25 20:36:44 +0200

    Remove logical SQRT_X and CZ implementations (#257)

--- tests/experiments/test_unrot_surface_code_css.py
+++ tests/experiments/test_unrot_surface_code_css.py
@@ -5,7 +5,6 @@ from surface_sim.layouts import unrot_surface_code
 from surface_sim.experiments.unrot_surface_code_css import (
     memory_experiment,
     repeated_s_experiment,
-    repeated_sqrt_x_experiment,
     repeated_h_experiment,
     repeated_cnot_experiment,
 )
@@ -13,7 +12,6 @@ from surface_sim.models import NoiselessModel
 from surface_sim import Detectors
 from surface_sim.log_gates.unrot_surface_code_css import (
     set_fold_trans_s,
-    set_fold_trans_sqrt_x,
     set_trans_cnot,
     set_fold_trans_h,
 )
@@ -69,44 +67,6 @@ def test_repeated_s_experiment():
             layout=layout,
             detectors=detectors,
             num_s_gates=4,
-            num_rounds_per_gate=2,
-            anc_reset=False,
-            data_init={q: 0 for q in layout.data_qubits},
-            rot_basis=rot_basis,
-        )
-
-        assert isinstance(circuit, stim.Circuit)
-
-        # check that the detectors and logicals fulfill their
-        # conditions by building the stim diagram
-        dem = circuit.detector_error_model(allow_gauge_detectors=True)
-
-        num_coords = 0
-        anc_coords = {k: list(map(float, v)) for k, v in layout.anc_coords.items()}
-        for dem_instr in dem:
-            if dem_instr.type == "detector":
-                assert dem_instr.args_copy()[:-1] in anc_coords.values()
-                num_coords += 1
-
-        assert num_coords == dem.num_detectors
-
-    return
-
-
-def test_repeated_sqrt_x_experiment():
-    layout = unrot_surface_code(distance=3)
-    set_fold_trans_sqrt_x(layout, "D1")
-    model = NoiselessModel(layout.qubit_inds)
-    detectors = Detectors(
-        layout.anc_qubits, frame="post-gate", anc_coords=layout.anc_coords
-    )
-
-    for rot_basis in [True, False]:
-        circuit = repeated_sqrt_x_experiment(
-            model=model,
-            layout=layout,
-            detectors=detectors,
-            num_sqrt_x_gates=4,
             num_rounds_per_gate=2,
             anc_reset=False,
             data_init={q: 0 for q in layout.data_qubits},
