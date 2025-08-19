f5c120d5372f02cf202eca067e47d5d795643c57
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-08-19 21:51:28 +0200

    Add rot_surface_codes

--- tests/layouts/library/test_rot_surface_codes.py
+++ tests/layouts/library/test_rot_surface_codes.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from surface_sim import Layout
 from surface_sim.layouts import (
     rot_surface_code,
+    rot_surface_codes,
     rot_surface_code_rectangle,
     rot_surface_code_rectangles,
     rot_surface_stability_rectangle,
@@ -66,6 +67,35 @@ def test_rot_surface_code_rectangle(show_figures):
 
 
 def test_rot_surface_codes():
+    layouts = rot_surface_codes(2, distance=3)
+
+    assert isinstance(layouts, list)
+    assert len(layouts) == 2
+
+    layout_0, layout_1 = layouts
+    assert set(
+        [
+            "log_x_L0",
+            "log_z_L0",
+            "idle_L0",
+            "log_trans_cnot_L0_L1",
+            "log_trans_cnot_L1_L0",
+        ]
+    ) < set(layout_0.to_dict()["layout"][0].keys())
+    assert set(
+        [
+            "log_x_L1",
+            "log_z_L1",
+            "idle_L1",
+            "log_trans_cnot_L0_L1",
+            "log_trans_cnot_L1_L0",
+        ]
+    ) < set(layout_1.to_dict()["layout"][0].keys())
+
+    return
+
+
+def test_rot_surface_code_rectangles():
     layouts = rot_surface_code_rectangles(2, distance=3)
 
     assert isinstance(layouts, list)
